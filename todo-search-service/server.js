94af2f44c2e9b1bcb7ac0b3130891c88acd3f4c7
Duane Davis <duane.j.davis@gmail.com>
2025-12-22 20:30:45 -0600

    Added production images, prod docker

--- todo-search-service/server.js
+++ todo-search-service/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const elasticsearch = require('elasticsearch');
-const envProps = require('./env_props');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Client } = require("@elastic/elasticsearch");
+const envProps = require("./env_props");
 
 /////// Test curl /////////////////////////////////////////////////////////////////////////
 // curl --data "searchText=kids" http://localhost:3002/api/v1/search
@@ -12,62 +12,64 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 // Elasticsearch Client Setup //////////////////////////////////////////////////////////////////////////////////////////
-const elasticClient = new elasticsearch.Client({
-    hosts: [ envProps.elasticHost + ':' + envProps.elasticPort]
+const elasticClient = new Client({
+  node: `http://${envProps.elasticHost}:${envProps.elasticPort}`,
 });
+
 // Ping the client to be sure Elastic is up
-elasticClient.ping({
-    requestTimeout: 30000,
-}, function(error) {
-    if (error) {
-        console.error('Something went wrong with Elasticsearch: ' + error);
-    } else {
-        console.log('Elasticsearch client connected');
-    }
-});
+async function checkElasticsearch() {
+  try {
+    await elasticClient.ping();
+    console.log("Elasticsearch client connected");
+  } catch (error) {
+    console.error("Something went wrong with Elasticsearch: " + error);
+  }
+}
+
+checkElasticsearch();
 
 // Set up the API routes ///////////////////////////////////////////////////////////////////////////////////////////////
 
 const TODO_SEARCH_INDEX_NAME = "todos";
-const TODO_SEARCH_INDEX_TYPE = "todo";
 
 // Search all todos
-app.route('/api/v1/search').post((req, res) => {
-    const searchText = req.body.searchText;
+app.route("/api/v1/search").post(async (req, res) => {
+  const searchText = req.body.searchText;
 
-    console.log('CALLED POST api/v1/search with searchText=' + searchText);
+  console.log("CALLED POST api/v1/search with searchText=" + searchText);
 
-    // Perform the actual search passing in the index, the search query and the type
-    elasticClient.search({
-        index: TODO_SEARCH_INDEX_NAME,
-        type: TODO_SEARCH_INDEX_TYPE,
-        body: {
-            query: {
-                match: {
-                    todotext: searchText
-                }
-            }
-        }
-    })
-    .then(results => {
-        console.log('Search for "' + searchText + '" matched (' + results.hits.hits.length + ')');
-        res.send(results.hits.hits);
-    })
-    .catch(err => {
-        console.log(err);
-        res.send([]);
+  try {
+    // Perform the actual search (ES 8.x - no type parameter)
+    const results = await elasticClient.search({
+      index: TODO_SEARCH_INDEX_NAME,
+      query: {
+        match: {
+          todotext: searchText,
+        },
+      },
     });
-});
 
+    console.log(
+      'Search for "' +
+        searchText +
+        '" matched (' +
+        results.hits.hits.length +
+        ")"
+    );
+    res.send(results.hits.hits);
+  } catch (err) {
+    console.log("Search error:", err);
+    res.send([]);
+  }
+});
 
 // Start the server ////////////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(port, () => {
-    console.log('Todo Search Service started!');
+  console.log("Todo Search Service started!");
 });
-
